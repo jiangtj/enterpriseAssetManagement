f9d627703bd56d31ec71ad4b84305e84203f64db
MrTT <jiang.taojie@foxmail.com>
2017-02-05 23:02:25 +0800

    添加菜单

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,20 @@
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+    { path: '/', component:{ template: '<div>1111111</div>' } },
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+];
+
+const router = new VueRouter({
+    routes:routes
+});
+
 var App = new Vue({
     el:"#wrapper",
+    router:router,
     data:{
         baseUrl:baseUrl,
         user: sessionUser,
--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,13 +1,25 @@
 //todo 将menu数据存入数据库
 var AppMenu = [
     {
+        name:"首页",
+        icon:"fa-cogs",
+        url:"/"
+    },
+    {
+        name:"资源管理",
+        icon:"fa-cogs",
+        list:[
+            {name:"111111111111",url:"/bar"}
+        ]
+    },
+    {
         name:"系统管理",
         icon:"fa-cogs",
         list:[
-            {name:"用户管理",url:"#"},
-            {name:"角色管理",url:"#"},
+            {name:"用户管理",url:"/foo"},
+            {name:"角色管理",url:"/bar"},
             // {name:"权限菜单",url:"#"},
-            {name:"权限管理",url:"#"}
+            {name:"权限管理",url:"/foo"}
         ]
     }
 ];
