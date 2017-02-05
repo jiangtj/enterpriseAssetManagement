4f0bdecbd7768ca0a3b855ca7a8e6091aa84f927
MrTT <jiang.taojie@foxmail.com>
2017-02-05 21:15:36 +0800

    添加菜单

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,9 @@
+var App = new Vue({
+    el:"#wrapper",
+    data:{
+        baseUrl:baseUrl,
+        user: sessionUser,
+        menu:AppMenu,
+        alwaysTrue:true
+    }
+});
---
+++ src/main/resources/static/js/menu.js
@@ -0,0 +1,13 @@
+//todo 将menu数据存入数据库
+var AppMenu = [
+    {
+        name:"系统管理",
+        icon:"fa-cogs",
+        list:[
+            {name:"用户管理",url:"#"},
+            {name:"角色管理",url:"#"},
+            // {name:"权限菜单",url:"#"},
+            {name:"权限管理",url:"#"}
+        ]
+    }
+];
