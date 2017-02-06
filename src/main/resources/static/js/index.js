05b144d1ceb66a0486cba8b22b33bc67ccd250c3
jiangtj <116749895@qq.com>
2017-02-06 13:05:23 +0800

    实现浏览器vue路由懒加载

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,12 +1,46 @@
 
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
+const routes = [];
 
-const routes = [
-    { path: '/', component:{ template: '<div>1111111</div>' } },
-    { path: '/foo', component: Foo },
-    { path: '/bar', component: Bar }
-];
+function pushRoute(item){
+    var object = {
+        menu: item,
+        path: item.url,
+        component: function (resolve) {
+            //var obj = object;//对象引用
+            var url = item.staticUrl;//获取url
+            //alert(obj.menu.name);
+            var view = "";
+            $.ajax({
+                type: "get",
+                //url: obj.menu.staticUrl,
+                url:url,
+                async: false,
+                dataType: "text",
+                success: function (data) {
+                    view = data;
+                }
+            });
+            resolve({
+                template:view
+            })
+        }
+    };
+    routes.push(object);
+}
+
+function getRoutes(items) {
+    for (var i = 0; i < items.length ; i++){
+        var item = items[i];
+        if (item.list != null){
+            getRoutes(item.list);
+        }
+        if (item.url != null) {
+            pushRoute(item)
+        }
+    }
+}
+
+getRoutes(AppMenu);
 
 const router = new VueRouter({
     routes:routes
--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,25 +1,26 @@
 //todo 将menu数据存入数据库
-var AppMenu = [
+const AppMenu = [
     {
         name:"首页",
         icon:"fa-cogs",
-        url:"/"
+        url:"/",
+        staticUrl:"/model/index.vue"
     },
     {
         name:"资源管理",
         icon:"fa-cogs",
         list:[
-            {name:"111111111111",url:"/bar"}
+            {name:"111111111111",url:"/bar",staticUrl:"/model/xxxx/aaa.vue"}
         ]
     },
     {
         name:"系统管理",
         icon:"fa-cogs",
         list:[
-            {name:"用户管理",url:"/foo"},
-            {name:"角色管理",url:"/bar"},
-            // {name:"权限菜单",url:"#"},
-            {name:"权限管理",url:"/foo"}
+            {name:"用户管理",url:"/system/user",staticUrl:"/model/system/user.vue"},
+            {name:"角色管理",url:"/system/role",staticUrl:"/model/system/role.vue"},
+            // {name:"权限菜单",url:"#",staticUrl:"/model/system/aaa.vue"},
+            {name:"权限管理",url:"/system/permission",staticUrl:"/model/system/permission.vue"}
         ]
     }
 ];
