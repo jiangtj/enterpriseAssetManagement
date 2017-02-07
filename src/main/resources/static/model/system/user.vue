52c9d530e6acdacd8f6851803477ae81bd21b75e
MrTT <jiang.taojie@foxmail.com>
2017-02-07 21:44:22 +0800

    表格和表单基本样式

--- src/main/resources/static/model/system/user.vue
+++ src/main/resources/static/model/system/user.vue
@@ -1,108 +1,111 @@
+<div>
 
-<div class="row">
-    <div class="col-lg-12">
-        <div class="ibox float-e-margins">
-            <div class="ibox-title">
-                <h5>Custom responsive table </h5>
-                <div class="ibox-tools">
-                    <a class="collapse-link">
-                        <i class="fa fa-chevron-up"></i>
-                    </a>
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-wrench"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#">Config option 1</a>
-                        </li>
-                        <li><a href="#">Config option 2</a>
-                        </li>
-                    </ul>
-                    <a class="close-link">
-                        <i class="fa fa-times"></i>
-                    </a>
-                </div>
-            </div>
-            <div class="ibox-content">
-                <div class="row">
-                    <div class="col-sm-5 m-b-xs"><select class="input-sm form-control input-s-sm inline">
-                        <option value="0">Option 1</option>
-                        <option value="1">Option 2</option>
-                        <option value="2">Option 3</option>
-                        <option value="3">Option 4</option>
-                    </select>
-                    </div>
-                    <div class="col-sm-4 m-b-xs">
-                        <div data-toggle="buttons" class="btn-group">
-                            <label class="btn btn-sm btn-white"> <input type="radio" id="option1" name="options"> Day </label>
-                            <label class="btn btn-sm btn-white active"> <input type="radio" id="option2" name="options"> Week </label>
-                            <label class="btn btn-sm btn-white"> <input type="radio" id="option3" name="options"> Month </label>
+    <div class="row wrapper border-bottom white-bg page-heading">
+        <div class="col-lg-10">
+            <h2>Basic Form</h2>
+            <ol class="breadcrumb">
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a>Forms</a>
+                </li>
+                <li class="active">
+                    <strong>Basic Form</strong>
+                </li>
+            </ol>
+        </div>
+        <div class="col-lg-2">
+
+        </div>
+    </div>
+
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <!--<div class="ibox-title">
+                        <h5>Custom responsive table </h5>
+                    </div>-->
+                    <div class="ibox-content">
+                        <form role="form" class="form-inline">
+                            <div class="form-group">
+                                <label for="exampleInputEmail2">Email address</label>
+                                <input type="email" placeholder="Enter email" id="exampleInputEmail2"
+                                       class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword2">Password</label>
+                                <input type="password" placeholder="Password" id="exampleInputPassword2"
+                                       class="form-control">
+                            </div>
+                            <button class="btn btn-white" type="submit">Sign in</button>
+                        </form>
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Project </th>
+                                    <th>Completed </th>
+                                    <th>Task</th>
+                                    <th>Date</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td><input type="checkbox"  checked class="i-checks icheckbox_square-green" name="input[]"></td>
+                                    <td>Project<small>This is example of project</small></td>
+                                    <td><span class="pie">0.52/1.561</span></td>
+                                    <td>20%</td>
+                                    <td>Jul 14, 2013</td>
+                                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" class="i-checks" name="input[]"></td>
+                                    <td>Alpha project</td>
+                                    <td><span class="pie">6,9</span></td>
+                                    <td>40%</td>
+                                    <td>Jul 16, 2013</td>
+                                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" class="i-checks" name="input[]"></td>
+                                    <td>Betha project</td>
+                                    <td><span class="pie">3,1</span></td>
+                                    <td>75%</td>
+                                    <td>Jul 18, 2013</td>
+                                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" class="i-checks" name="input[]"></td>
+                                    <td>Gamma project</td>
+                                    <td><span class="pie">4,9</span></td>
+                                    <td>18%</td>
+                                    <td>Jul 22, 2013</td>
+                                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
+                                </tr>
+                                </tbody>
+                            </table>
                         </div>
-                    </div>
-                    <div class="col-sm-3">
-                        <div class="input-group"><input type="text" placeholder="Search" class="input-sm form-control"> <span class="input-group-btn">
-                                        <button type="button" class="btn btn-sm btn-primary"> Go!</button> </span></div>
+
                     </div>
                 </div>
-                <div class="table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-
-                            <th></th>
-                            <th>Project </th>
-                            <th>Completed </th>
-                            <th>Task</th>
-                            <th>Date</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td><input type="checkbox"  checked class="i-checks" name="input[]"></td>
-                            <td>Project<small>This is example of project</small></td>
-                            <td><span class="pie">0.52/1.561</span></td>
-                            <td>20%</td>
-                            <td>Jul 14, 2013</td>
-                            <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox" class="i-checks" name="input[]"></td>
-                            <td>Alpha project</td>
-                            <td><span class="pie">6,9</span></td>
-                            <td>40%</td>
-                            <td>Jul 16, 2013</td>
-                            <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox" class="i-checks" name="input[]"></td>
-                            <td>Betha project</td>
-                            <td><span class="pie">3,1</span></td>
-                            <td>75%</td>
-                            <td>Jul 18, 2013</td>
-                            <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox" class="i-checks" name="input[]"></td>
-                            <td>Gamma project</td>
-                            <td><span class="pie">4,9</span></td>
-                            <td>18%</td>
-                            <td>Jul 22, 2013</td>
-                            <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-
             </div>
         </div>
     </div>
-</div>
 
-<script>
-    $(document).ready(function(){
-        $('.i-checks').iCheck({
-            checkboxClass: 'icheckbox_square-green',
-            radioClass: 'iradio_square-green',
-        });
-    });
-</script>
+    <script type="application/javascript">
+        //todo 移入common.js
+        function beautifyChecks() {
+            $('.i-checks').iCheck({
+                checkboxClass: 'icheckbox_square-green',
+                radioClass: 'iradio_square-green'
+            });
+        }
+        //美化复选框
+        beautifyChecks();
+    </script>
+
+</div>
