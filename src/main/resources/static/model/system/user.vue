3ea4a97f8d62182315b230972b3ecd0c5fd6ea1d
MrTT <jiang.taojie@foxmail.com>
2017-02-08 20:58:17 +0800

    表格和表单基本样式

--- src/main/resources/static/model/system/user.vue
+++ src/main/resources/static/model/system/user.vue
@@ -1,5 +1,6 @@
 <div>
 
+    <!-- 头部标签 -->
     <header-label></header-label>
     <!--<div class="row wrapper border-bottom white-bg page-heading">
         <div class="col-lg-10">
@@ -13,95 +14,89 @@
         <div class="col-lg-2"></div>
     </div>-->
 
+    <!-- 动画 -->
     <div class="wrapper wrapper-content animated fadeInRight">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="ibox from-table-margins">
-                    <!--<div class="ibox-title">
-                        <h5>Custom responsive table </h5>
-                    </div>-->
-                    <div class="ibox-content">
-                        <form role="form" class="form-inline">
-                            <div class="form-group from-input-margins">
-                                <label for="exampleInputEmail2">Email address</label>
-                                <input type="email" placeholder="Enter email" id="exampleInputEmail2"
-                                       class="form-control">
-                            </div>
-                            <div class="form-group from-input-margins">
-                                <label for="exampleInputPassword2">Password</label>
-                                <input type="password" placeholder="Password" id="exampleInputPassword2"
-                                       class="form-control">
-                            </div>
-                            <button class="btn btn-white from-button-style" type="submit">Sign in</button>
-                            <div class="clearfix"></div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="ibox float-e-margins">
-                    <!--<div class="ibox-title">
-                        <h5>Custom responsive table </h5>
-                    </div>-->
-                    <div class="ibox-content">
-                        <div class="table-responsive">
-                            <table class="table table-striped">
-                                <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Project </th>
-                                    <th>Completed </th>
-                                    <th>Task</th>
-                                    <th>Date</th>
-                                    <th>Action</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td><input type="checkbox"  checked class="i-checks icheckbox_square-green" name="input[]"></td>
-                                    <td>Project<small>This is example of project</small></td>
-                                    <td><span class="pie">0.52/1.561</span></td>
-                                    <td>20%</td>
-                                    <td>Jul 14, 2013</td>
-                                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                                </tr>
-                                <tr>
-                                    <td><input type="checkbox" class="i-checks" name="input[]"></td>
-                                    <td>Alpha project</td>
-                                    <td><span class="pie">6,9</span></td>
-                                    <td>40%</td>
-                                    <td>Jul 16, 2013</td>
-                                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                                </tr>
-                                <tr>
-                                    <td><input type="checkbox" class="i-checks" name="input[]"></td>
-                                    <td>Betha project</td>
-                                    <td><span class="pie">3,1</span></td>
-                                    <td>75%</td>
-                                    <td>Jul 18, 2013</td>
-                                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                                </tr>
-                                <tr>
-                                    <td><input type="checkbox" class="i-checks" name="input[]"></td>
-                                    <td>Gamma project</td>
-                                    <td><span class="pie">4,9</span></td>
-                                    <td>18%</td>
-                                    <td>Jul 22, 2013</td>
-                                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
 
+        <!-- 表单 -->
+        <div class="row"><div class="col-lg-12"><div class="ibox tt-from-table">
+            <!--<div class="ibox-title">
+                <h5>Custom responsive table </h5>
+            </div>-->
+            <div class="ibox-content">
+                <form role="form" class="form-inline">
+                    <div class="form-group tt-from-input">
+                        <label for="exampleInputEmail2">Email address</label>
+                        <input type="email" placeholder="Enter email" id="exampleInputEmail2"
+                               class="form-control">
                     </div>
+                    <div class="form-group tt-from-input">
+                        <label for="exampleInputPassword2">Password</label>
+                        <input type="password" placeholder="Password" id="exampleInputPassword2"
+                               class="form-control">
+                    </div>
+                    <button class="btn btn-white pull-right" type="submit">Sign in</button>
+                    <div class="clearfix"></div>
+                </form>
+            </div>
+        </div></div></div>
+
+        <!-- 表格 -->
+        <div class="row"><div class="col-lg-12"><div class="ibox float-e-margins">
+            <div class="ibox-content">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th></th>
+                            <th>Project </th>
+                            <th>Completed </th>
+                            <th>Task</th>
+                            <th>Date</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td><input type="checkbox"  checked class="i-checks icheckbox_square-green" name="input[]"></td>
+                            <td>Project<small>This is example of project</small></td>
+                            <td><span class="pie">0.52/1.561</span></td>
+                            <td>20%</td>
+                            <td>Jul 14, 2013</td>
+                            <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td><input type="checkbox" class="i-checks" name="input[]"></td>
+                            <td>Alpha project</td>
+                            <td><span class="pie">6,9</span></td>
+                            <td>40%</td>
+                            <td>Jul 16, 2013</td>
+                            <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td><input type="checkbox" class="i-checks" name="input[]"></td>
+                            <td>Betha project</td>
+                            <td><span class="pie">3,1</span></td>
+                            <td>75%</td>
+                            <td>Jul 18, 2013</td>
+                            <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td><input type="checkbox" class="i-checks" name="input[]"></td>
+                            <td>Gamma project</td>
+                            <td><span class="pie">4,9</span></td>
+                            <td>18%</td>
+                            <td>Jul 22, 2013</td>
+                            <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        </div>
+        </div></div></div>
     </div>
 
     <script type="application/javascript">
+
         //美化复选框
         ICheckUtils.beautifyChecks();
 
