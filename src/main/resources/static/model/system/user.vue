6ea42ff907aa762266aa3ec0dac268b659a711fc
jiangtj <116749895@qq.com>
2017-02-13 18:27:47 +0800

    ......

--- src/main/resources/static/model/system/user.vue
+++ src/main/resources/static/model/system/user.vue
@@ -3,17 +3,6 @@
 
         <!-- 头部标签 -->
         <header-label :data="headerLabel"></header-label>
-        <!--<div class="row wrapper border-bottom white-bg page-heading">
-            <div class="col-lg-10">
-                <h2>用户管理</h2>
-                <ol class="breadcrumb">
-                    <li><router-link to="/">Home</router-link></li>
-                    <li><a>System</a></li>
-                    <li class="active"><strong>User</strong></li>
-                </ol>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>-->
 
         <!-- 动画 -->
         <div class="wrapper wrapper-content animated fadeInRight">
@@ -51,57 +40,6 @@
                                 {{props.index}}-{{props.row.id}}
                             </template>
                         </tt-table>
-                        <!--<tt-table v-bind:data="tableDefaultData">
-                            <tt-table-column prop="id" name="id"></tt-table-column>
-                            <tt-table-column prop="name" name="名称"></tt-table-column>
-                            <tt-table-column prop="password" name="密码"></tt-table-column>
-                        </tt-table>-->
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th></th>
-                                <th>Project </th>
-                                <th>Completed </th>
-                                <th>Task</th>
-                                <th>Date</th>
-                                <th>Action</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td><input type="checkbox"  checked class="i-checks icheckbox_square-green" name="input[]"></td>
-                                <td>Project<small>This is example of project</small></td>
-                                <td><span class="pie">0.52/1.561</span></td>
-                                <td>20%</td>
-                                <td>Jul 14, 2013</td>
-                                <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                            </tr>
-                            <tr>
-                                <td><input type="checkbox" class="i-checks" name="input[]"></td>
-                                <td>Alpha project</td>
-                                <td><span class="pie">6,9</span></td>
-                                <td>40%</td>
-                                <td>Jul 16, 2013</td>
-                                <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                            </tr>
-                            <tr>
-                                <td><input type="checkbox" class="i-checks" name="input[]"></td>
-                                <td>Betha project</td>
-                                <td><span class="pie">3,1</span></td>
-                                <td>75%</td>
-                                <td>Jul 18, 2013</td>
-                                <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                            </tr>
-                            <tr>
-                                <td><input type="checkbox" class="i-checks" name="input[]"></td>
-                                <td>Gamma project</td>
-                                <td><span class="pie">4,9</span></td>
-                                <td>18%</td>
-                                <td>Jul 22, 2013</td>
-                                <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                            </tr>
-                            </tbody>
-                        </table>
                     </div>
                 </div>
             </div></div></div>
@@ -159,23 +97,9 @@
             }
         },
         mounted:function () {
-            //美化复选框
-            //ICheckUtils.beautifyChecks();
         },
         methods: {
         }
     };
-    /*RouteData.tableDefaultData={
-        title:{
-            id:"id",
-            name:"名称",
-            password:"密码"
-        },
-        data:[
-            {id:1,name:"dfbdf",password:"fdg"},
-            {id:22,name:"scvbs1",password:"s"},
-            {id:88,name:"rg",password:"s"}
-        ]
-    };*/
 
 </script>
