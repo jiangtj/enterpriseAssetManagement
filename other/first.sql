64c36e796a2fbd9a35b246af2ac2fe6b030cc1cc
MrTT <jiang.taojie@foxmail.com>
2017-03-12 22:07:28 +0800

    sql修改

--- other/first.sql
+++ other/first.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : mysql-wamp
+Source Server         : mysql-mine
 Source Server Version : 50714
 Source Host           : localhost:3306
 Source Database       : first
@@ -10,10 +10,101 @@ Target Server Type    : MYSQL
 Target Server Version : 50714
 File Encoding         : 65001
 
-Date: 2017-01-31 00:20:37
+Date: 2017-03-12 22:03:20
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for assets_borrow
+-- ----------------------------
+DROP TABLE IF EXISTS `assets_borrow`;
+CREATE TABLE `assets_borrow` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `uuid` char(36) NOT NULL COMMENT 'uuid',
+  `user_id` bigint(20) NOT NULL COMMENT '租借人id',
+  `expect_return_time` datetime DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL COMMENT '资产名称',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique@uuid` (`uuid`) USING BTREE,
+  KEY `ab@user_id` (`user_id`) USING BTREE,
+  CONSTRAINT `ab@user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`),
+  CONSTRAINT `ab@uuid` FOREIGN KEY (`uuid`) REFERENCES `assets_item` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for assets_item
+-- ----------------------------
+DROP TABLE IF EXISTS `assets_item`;
+CREATE TABLE `assets_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `uuid` char(36) NOT NULL COMMENT 'uuid',
+  `customs_id` varchar(50) NOT NULL COMMENT '用户提供的资产编码',
+  `name` varchar(255) NOT NULL COMMENT '资产名称',
+  `price` decimal(10,2) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态，1：正常，2：租借，3：维修，4：报废',
+  `assets_type_id` bigint(20) NOT NULL COMMENT '资产类型id',
+  `point_id` bigint(20) NOT NULL COMMENT '网点id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique@uuid` (`uuid`) USING BTREE,
+  KEY `ai@point_id` (`point_id`) USING BTREE,
+  KEY `ai@assets_type_id` (`assets_type_id`) USING BTREE,
+  CONSTRAINT `ai@assets_type_id` FOREIGN KEY (`assets_type_id`) REFERENCES `assets_type` (`id`),
+  CONSTRAINT `ai@point_id` FOREIGN KEY (`point_id`) REFERENCES `point` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for assets_operation_record
+-- ----------------------------
+DROP TABLE IF EXISTS `assets_operation_record`;
+CREATE TABLE `assets_operation_record` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `uuid` char(36) NOT NULL COMMENT 'uuid',
+  `user_id` bigint(20) NOT NULL COMMENT '操作人id',
+  `operation_type` tinyint(4) NOT NULL COMMENT '操作类型，1：登记，2：借，3：换，4：丢失，5：报修，6：作废，7：盘点',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for assets_stock_take
+-- ----------------------------
+DROP TABLE IF EXISTS `assets_stock_take`;
+CREATE TABLE `assets_stock_take` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `no` varchar(50) NOT NULL COMMENT '盘点编号',
+  `name` varchar(50) NOT NULL COMMENT '盘点名称',
+  `user_id` bigint(20) NOT NULL COMMENT '盘点负责人',
+  `end_time` datetime DEFAULT NULL COMMENT '盘点结束时间',
+  `all_amount` int(8) DEFAULT '0' COMMENT '盘点数目',
+  `handling_amount` int(8) DEFAULT '0' COMMENT '待处理数目',
+  `normal_amount` int(8) DEFAULT '0' COMMENT '正常数目',
+  `abnormal_amount` int(8) DEFAULT '0' COMMENT '异常数目',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for assets_type
+-- ----------------------------
+DROP TABLE IF EXISTS `assets_type`;
+CREATE TABLE `assets_type` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '类型id',
+  `name` varchar(50) NOT NULL COMMENT '类型名称',
+  `level` int(8) NOT NULL COMMENT '级别',
+  `pid` bigint(20) NOT NULL COMMENT '父节点id',
+  `order` int(8) NOT NULL COMMENT '同一个父节点下面的排序',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for auth_menu
@@ -32,7 +123,7 @@ CREATE TABLE `auth_menu` (
   PRIMARY KEY (`id`),
   KEY `menu@permission_id` (`permission_id`),
   CONSTRAINT `menu@permission_id` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for auth_permission
@@ -61,6 +152,23 @@ CREATE TABLE `auth_role` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Table structure for auth_role_assets_type_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_role_assets_type_relation`;
+CREATE TABLE `auth_role_assets_type_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `role_id` bigint(20) NOT NULL COMMENT '角色id',
+  `assets_type_id` bigint(20) NOT NULL COMMENT '资产类型id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique@role_id, assets_type_id` (`role_id`,`assets_type_id`) USING BTREE,
+  KEY `rat@assets_type_id` (`assets_type_id`) USING BTREE,
+  CONSTRAINT `rat@assets_type_id` FOREIGN KEY (`assets_type_id`) REFERENCES `assets_type` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `rat@role_id` FOREIGN KEY (`role_id`) REFERENCES `auth_role` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Table structure for auth_role_permission_relation
 -- ----------------------------
 DROP TABLE IF EXISTS `auth_role_permission_relation`;
@@ -78,6 +186,23 @@ CREATE TABLE `auth_role_permission_relation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Table structure for auth_role_point_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_role_point_relation`;
+CREATE TABLE `auth_role_point_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `role_id` bigint(20) NOT NULL COMMENT '角色id',
+  `point_id` bigint(20) NOT NULL COMMENT '网点id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique@role_id, point_id` (`role_id`,`point_id`) USING BTREE,
+  KEY `rp1@point_id` (`point_id`) USING BTREE,
+  CONSTRAINT `rp1@point_id` FOREIGN KEY (`point_id`) REFERENCES `point` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `rp1@role_id` FOREIGN KEY (`role_id`) REFERENCES `auth_role` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Table structure for auth_user
 -- ----------------------------
 DROP TABLE IF EXISTS `auth_user`;
@@ -89,6 +214,36 @@ CREATE TABLE `auth_user` (
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`),
+  UNIQUE KEY `unique@name` (`name`) USING BTREE,
   KEY `user@role_id` (`role_id`),
   CONSTRAINT `user@role_id` FOREIGN KEY (`role_id`) REFERENCES `auth_role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for point
+-- ----------------------------
+DROP TABLE IF EXISTS `point`;
+CREATE TABLE `point` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '网点id',
+  `name` varchar(50) NOT NULL COMMENT '网点名称',
+  `level` int(8) NOT NULL COMMENT '级别',
+  `pid` bigint(20) NOT NULL COMMENT '父节点id',
+  `order` int(8) NOT NULL COMMENT '同一个父节点下面的排序',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for system_config
+-- ----------------------------
+DROP TABLE IF EXISTS `system_config`;
+CREATE TABLE `system_config` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `variable` varchar(128) NOT NULL COMMENT '系统变量',
+  `value` varchar(128) DEFAULT NULL COMMENT '系统值',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique@variable` (`variable`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
