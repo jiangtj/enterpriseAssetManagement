b1f099ee9d6417f0608ab6fafd334b1f38c7e0f2
MrTT <jiang.taojie@foxmail.com>
2017-05-05 00:08:25 +0800

    sql

--- other/update.sql
+++ other/update.sql
@@ -30,3 +30,7 @@ ALTER TABLE `assets_stock_take`
   ADD COLUMN `status`  tinyint(4) NULL DEFAULT 1 COMMENT '状态，1：盘点中，2：盘点完成' AFTER `user_id`;
 INSERT INTO `asset`.`system_dictionary` (`id`, `table`, `column`, `key`, `value`, `create_time`, `update_time`) VALUES ('19', 'assets_stock_take', 'status', '1', '盘点中', '2017-05-04 23:43:05', '2017-05-04 23:43:05');
 INSERT INTO `asset`.`system_dictionary` (`id`, `table`, `column`, `key`, `value`, `create_time`, `update_time`) VALUES ('20', 'assets_stock_take', 'status', '2', '盘点完成', '2017-05-04 23:43:05', '2017-05-04 23:43:05');
+
+UPDATE `asset`.`auth_menu` SET `id`='32', `name`='任务', `menu`='StockTakeTask', `level`='2', `pid`='31', `order`='1', `type`='1', `icon`=NULL, `url`='/stockTake/task', `static_url`='/model/stockTake/task.vue', `permission_id`=NULL, `create_time`='2017-05-04 16:05:36', `update_time`='2017-05-04 22:46:29' WHERE (`id`='32');
+UPDATE `asset`.`auth_menu` SET `id`='33', `name`='处理', `menu`='StockTakeHandle', `level`='2', `pid`='31', `order`='3', `type`='1', `icon`=NULL, `url`='/stockTake/handle', `static_url`='/model/stockTake/handle.vue', `permission_id`=NULL, `create_time`='2017-05-04 16:21:28', `update_time`='2017-05-04 22:46:34' WHERE (`id`='33');
+INSERT INTO `asset`.`auth_menu` (`id`, `name`, `menu`, `level`, `pid`, `order`, `type`, `icon`, `url`, `static_url`, `permission_id`, `create_time`, `update_time`) VALUES ('34', '明细', 'StockTakeItem', '2', '31', '2', '1', NULL, '/stockTake/item', '/model/stockTake/item.vue', NULL, '2017-05-04 22:46:10', '2017-05-04 22:46:38');
