87eeadbbae5fe1b044e91f36302769a5f742816f
jiangtj <116749895@qq.com>
2017-05-05 11:11:07 +0800

    index

--- src/main/resources/static/model/index.vue
+++ src/main/resources/static/model/index.vue
@@ -4,10 +4,10 @@
             <div class="col-lg-12">
                 <div class="text-center m-t-lg">
                     <h1>
-                        Welcome in INSPINIA Static SeedProject
+                        企业固定资产管理系统
                     </h1>
                     <small>
-                        It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects and dev environment for these projects.
+                        从购置、领用、盘点、借用归还、维修到报废进行全方位准确监管，以“快捷”、“精准”和功能全面为优势的管理类系统.
                     </small>
                 </div>
             </div>
--- src/main/resources/static/model/xxxx/aaa.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>asdasd</div>
-</template>
